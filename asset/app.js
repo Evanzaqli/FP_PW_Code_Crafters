dcedf8e2e26bfd5226bee2bcbe4e2984068fc8f7
Evan Zaqli <evanzaqli1@gmail.com>
2024-05-17 13:58:16 +0700

    about Team

---
+++ asset/app.js
@@ -0,0 +1,19 @@
+/* js swiper */
+
+let swiper = new Swiper(".wrapper", {
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: false,
+    },
+  });
